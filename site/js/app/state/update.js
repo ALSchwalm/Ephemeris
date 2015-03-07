4dd1322dcd07892bc15cc0aa443d32a649049644
Adam Schwalm <adamschwalm@gmail.com>
2015-03-07 14:22:14 -0600

    Add game over screen

--- site/js/app/state/update.js
+++ site/js/app/state/update.js
@@ -2,11 +2,31 @@
  * A module returning a function which will be executed during each frame
  * @module app/state/update
  */
-define(["app/controls", "app/interface", "app/fog", "app/action"],
-function(controls, hud, fog, action){
+define(["app/controls", "app/interface", "app/fog",
+        "app/action", "app/map", "app/player"],
+function(controls, hud, fog, action, map, player){
     "use strict"
 
-    var active = false;
+    var gameIsOver = function(game) {
+        var unitPlayer = null;
+        for (var i=0; i < game.units.length; ++i) {
+            if (!unitPlayer) {
+                unitPlayer = game.units[i].owner;
+            } else if (game.units[i].owner &&
+                       unitPlayer != game.units[i].owner) {
+                // There are units from two different players
+                return null;
+            }
+        }
+
+        for (var i=0; i < map.controlPoints.length; ++i) {
+            if (map.controlPoints[i].owner != unitPlayer) {
+                // There is a control point owned by a different player
+                return null;
+            }
+        }
+        return unitPlayer;
+    }
 
     /**
      * Function which will be executed by Phaser during each frame
@@ -20,6 +40,17 @@ function(controls, hud, fog, action){
             fog.update();
             hud.update();
             action.update();
+
+            var winner = gameIsOver(game);
+            if (winner) {
+                game.running = false;
+                if (winner == player) {
+                    $("#game-over h1").html("Victory");
+                } else {
+                    $("#game-over h1").html("Defeat");
+                }
+                $("#game-over").removeClass("hidden");
+            }
         }
     }
 
